948fbb30252d0f8949c15b142ce1c65c7c3c4287
Peter N Jenkin <peternjenkin@gmail.com>
2019-04-25 09:58:49 +0100

    10 State (Life Cycle) log messages added

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    // bespoke, for logging state
+    private static final String TAG = "pnjMessage";     // TAG is conventional name
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,61 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Log.i(TAG, "onCreate");
     }
+
+    // Alt+Insert code generation - Overrides ....
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "Start"); // copy/pasted
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle outState) {
+        super.onRestoreInstanceState(outState);
+
+        Log.i(TAG, "onRestoreInstanceState");
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
